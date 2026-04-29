2527bfe2ab391195a6e6bf5206489bb6f1b21796
Daniel Oliveira <danieloliveirasilva1905@gmail.com>
2026-04-29 10:32:27 -0300

    Create Funcionario.java

---
+++ Funcionario.java
@@ -0,0 +1,52 @@
+public abstract class  Funcionario {
+
+    private String nome;
+    private String cpf;
+    private double salarioBase;
+
+    public Funcionario(String nome, String cpf, double salarioBase) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.salarioBase = salarioBase;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public double getSalarioBase() {
+        return salarioBase;
+    }
+
+
+    public void setSalarioBase(double salarioBase) {
+        if (salarioBase > 0) {
+            this.salarioBase = salarioBase;
+        }else {
+            System.out.println("Erro: Sálario base deve ser positivo!");
+
+        }
+    }
+
+    public abstract double calcularSalario();
+
+    public abstract String getTipoFuncionario();
+
+    }
+
+
+
+
+
